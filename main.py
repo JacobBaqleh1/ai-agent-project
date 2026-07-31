777808dfbe307ebd97d738bad19ba53bfae167cf
root <root@DESKTOP-3C0U318.localdomain>
2026-07-31 21:01:40 +0000

    added a flag using argparse python package

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os   
+import argparse
 from dotenv import load_dotenv
 from openai import OpenAI 
 
@@ -10,18 +11,28 @@ client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
     api_key=api_key,
 )
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
+
+messages = [
+    {"role": "user", "content": args.user_prompt},
+]
 
 response = client.chat.completions.create(
     model='openrouter/free',
-    messages=[
-        {
-"role": "user",
-"content": "Why is the sky blue"
-        }
-    ],
+    messages=messages,
 )
+
+
+
 if response.usage is None:
     raise RuntimeError("API request failed: usage metadata missing")
-print(f"Prompt tokens:{response.usage.prompt_tokens}")    
-print(f"Response tokens: {response.usage.completion_tokens}")
+
+if args.verbose:
+    print(f"User prompt:{args.user_prompt}")
+    print(f"Prompt tokens:{response.usage.prompt_tokens}")    
+    print(f"Response tokens: {response.usage.completion_tokens}")
+
 print(response.choices[0].message.content)
